1b267f94afa37075fb5b2f01919a6556070cc1b3
Aniketwagh123 <106876220+Aniketwagh123@users.noreply.github.com>
2024-07-15 21:49:46 +0530

    [Aniket Wagh]Feat: Implemented part-time employee wage calculation

--- employee_wage.py
+++ employee_wage.py
@@ -2,16 +2,22 @@ import random
 
 WAGE_PER_HOUR = 20
 FULL_DAY_HOUR = 8
+PART_TIME_HOUR = 4
 
+def calculate_wage(hours):
+    return hours * WAGE_PER_HOUR
 
 def check_attendance():
-    return random.choice([True, False])
-
+    return random.choice(["full_time", "part_time", "absent"])
 
 if __name__ == "__main__":
     print("Welcome to Employee Wage Computation Program")
-    if check_attendance():
-        daily_wage = WAGE_PER_HOUR * FULL_DAY_HOUR
-        print(f"Daily Employee Wage: {daily_wage}")
+    employee_type = check_attendance()
+    if employee_type == "full_time":
+        wage = calculate_wage(FULL_DAY_HOUR)
+    elif employee_type == "part_time":
+        wage = calculate_wage(PART_TIME_HOUR)
     else:
-        print("Employee is Absent")
+        wage = 0
+
+    print(f"Employee Wage: {wage}")
