79579f0e1d67f4ddf9ad54db1fc81da22d724404
Aniketwagh123 <106876220+Aniketwagh123@users.noreply.github.com>
2024-07-15 21:51:32 +0530

    [Aniket Wagh]Feat: Implemented match case for employee wage calculation

--- employee_wage.py
+++ employee_wage.py
@@ -4,20 +4,22 @@ WAGE_PER_HOUR = 20
 FULL_DAY_HOUR = 8
 PART_TIME_HOUR = 4
 
+
 def calculate_wage(hours):
     return hours * WAGE_PER_HOUR
 
-def check_attendance():
-    return random.choice(["full_time", "part_time", "absent"])
+
+def get_wage(employee_type):
+    match employee_type:
+        case 'full_time':
+            return calculate_wage(FULL_DAY_HOUR)
+        case 'part_time':
+            return calculate_wage(PART_TIME_HOUR)
+        case 'absent':
+            return 0
+
 
 if __name__ == "__main__":
     print("Welcome to Employee Wage Computation Program")
-    employee_type = check_attendance()
-    if employee_type == "full_time":
-        wage = calculate_wage(FULL_DAY_HOUR)
-    elif employee_type == "part_time":
-        wage = calculate_wage(PART_TIME_HOUR)
-    else:
-        wage = 0
-
-    print(f"Employee Wage: {wage}")
+    employee_type = random.choice(['full_time', 'part_time', 'absent'])
+    print(f"Employee Wage: {get_wage(employee_type)}")
