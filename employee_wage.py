387bf5914053839664b9e873c918fe7bdbfc045b
Aniketwagh123 <106876220+Aniketwagh123@users.noreply.github.com>
2024-07-15 22:02:11 +0530

    [Aniket Wagh]Feat: Implemented wage calculation for multiple companies

--- employee_wage.py
+++ employee_wage.py
@@ -1,13 +1,12 @@
 import random
 
 class EmployeeWage:
-    WAGE_PER_HOUR = 20
-    FULL_DAY_HOUR = 8
-    PART_TIME_HOUR = 4
-    MAX_HOURS_IN_MONTH = 100
-    MAX_DAYS_IN_MONTH = 20
-
-    def __init__(self):
+    def __init__(self, wage_per_hour, max_hours_in_month, max_days_in_month):
+        self.WAGE_PER_HOUR = wage_per_hour
+        self.FULL_DAY_HOUR = 8
+        self.PART_TIME_HOUR = 4
+        self.MAX_HOURS_IN_MONTH = max_hours_in_month
+        self.MAX_DAYS_IN_MONTH = max_days_in_month
         self.total_wage = 0
         self.total_hours = 0
         self.total_days = 0
@@ -26,7 +25,7 @@ class EmployeeWage:
 
     def add_wage(self, employee_type):
         hours_worked = self.FULL_DAY_HOUR if employee_type == 'full_time' else self.PART_TIME_HOUR
-        if self.total_hours + hours_worked <= self.MAX_HOURS_IN_MONTH and self.total_days + 1 <= self.MAX_DAYS_IN_MONTH:
+        if  self.total_hours + hours_worked <= self.MAX_HOURS_IN_MONTH and self.total_days + 1 <= self.MAX_DAYS_IN_MONTH:
             self.total_hours += hours_worked
             self.total_wage += self.get_wage(employee_type)
             self.total_days += 1
@@ -39,8 +38,15 @@ class EmployeeWage:
 
 if __name__ == "__main__":
     print("Welcome to Employee Wage Computation Program")
-    employee = EmployeeWage()
-    while employee.total_hours < EmployeeWage.MAX_HOURS_IN_MONTH and employee.total_days < EmployeeWage.MAX_DAYS_IN_MONTH:
-        employee_type = employee.check_attendance()
-        employee.add_wage(employee_type)
-    print(f"Total Wage for the Month: {employee.get_total_wage()}")
+    companies = [
+        {"name": "Company A", "wage_per_hour": 20, "max_hours_in_month": 100, "max_days_in_month": 20},
+        {"name": "Company B", "wage_per_hour": 25, "max_hours_in_month": 120, "max_days_in_month": 22}
+    ]
+
+    for company in companies:
+        print(f"\nCalculating wages for {company['name']}")
+        employee = EmployeeWage(company['wage_per_hour'], company['max_hours_in_month'], company['max_days_in_month'])
+        while employee.total_hours < employee.MAX_HOURS_IN_MONTH and employee.total_days < employee.MAX_DAYS_IN_MONTH:
+            employee_type = employee.check_attendance()
+            employee.add_wage(employee_type)
+        print(f"Total Wage for the Month for {company['name']}: {employee.get_total_wage()}")
