b51b3236d1bce6ff658472c39c423886546935fb
Aniketwagh123 <106876220+Aniketwagh123@users.noreply.github.com>
2024-07-15 21:45:49 +0530

    [Aniket Wagh]Feat: Implemented employee attendance check

--- employee_wage.py
+++ employee_wage.py
@@ -1,1 +1,13 @@
-print("Welcome to Employee Wage Computation Program")
+import random
+
+
+def check_attendance():
+    return random.choice([True, False])
+
+
+if __name__ == "__main__":
+    print("Welcome to Employee Wage Computation Program")
+    if check_attendance():
+        print("Employee is Present")
+    else:
+        print("Employee is Absent")
